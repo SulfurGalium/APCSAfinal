ca1fad8b8425acce9b6c873deeb294fe374164e5
SulfurGalium <106123374+SulfurGalium@users.noreply.github.com>
2026-04-29 12:14:57 -0700

    starting jump
    not working yet, creating a floor and a camera that is attached to a player object that moves based on key inputs

--- game/src/main/java/com/finalproj/finalproject/game/FinalProject.java
+++ game/src/main/java/com/finalproj/finalproject/game/FinalProject.java
@@ -3,9 +3,18 @@ package com.finalproj.finalproject.game;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.app.state.AppState;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.scene.Node;
+
 
 /**
  * The JMonkeyEngine game entry, you should only do initializations for your game here, game logic is handled by
@@ -13,7 +22,12 @@ import com.jme3.app.state.AppState;
  * and your custom entities implementations of the previous.
  *
  */
-public class FinalProject extends SimpleApplication {
+public class FinalProject extends SimpleApplication implements ActionListener {
+
+    BulletAppState bulletAppState;
+    BetterCharacterControl playerControl;
+    Node playerNode;
+    
 
     public FinalProject() {
     }
@@ -22,16 +36,91 @@ public class FinalProject extends SimpleApplication {
         super(initialStates);
     }
 
+    private void initKeys() {
+        inputManager.addMapping("Jump",  new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addMapping("Left",  new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Up",    new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down",  new KeyTrigger(KeyInput.KEY_S));
+    
+
+        inputManager.addListener(this, "Jump", "Left", "Right", "Up", "Down");
+    }
+    
+
+    private Vector3f walkDirection = new Vector3f();
+    private boolean left = false, right = false, up = false, down = false;
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals("Jump") && isPressed) {
+            playerControl.jump();
+        }
+
+        if (name.equals("Left"))  { left  = isPressed; System.out.println("L");}
+        if (name.equals("Right")) { right = isPressed; }
+        if (name.equals("Up"))    { up    = isPressed; }
+        if (name.equals("Down"))  { down  = isPressed; }
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        Vector3f camDir = cam.getDirection().clone().setY(0).normalizeLocal();
+        Vector3f camLeft = cam.getLeft().clone().setY(0).normalizeLocal();
+        walkDirection.set(0, 0, 0);
+    
+        if (left)  { walkDirection.addLocal(camLeft); }
+        if (right) { walkDirection.addLocal(camLeft.negateLocal()); }
+        if (up)    { walkDirection.addLocal(camDir); }
+        if (down)  { walkDirection.addLocal(camDir.negateLocal()); }
+    
+        // This actually moves the physical character
+        playerControl.setWalkDirection(walkDirection.multLocal(15f)); 
+        
+        // Keep the camera locked to the player
+        cam.setLocation(playerNode.getWorldTranslation().add(0, 4f, 0));
+    }
+    
+
+
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        Box floorBox = new Box(10f, 0.1f, 10f);
+        Geometry floorGeo = new Geometry("Floor", floorBox);
+        Material floorMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        floorMat.setColor("Color", ColorRGBA.Blue);
+        floorGeo.setMaterial(floorMat);
+        floorGeo.setLocalTranslation(0, -0.1f, 0);
+        rootNode.attachChild(floorGeo);
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
+        RigidBodyControl floorPhy = new RigidBodyControl(0.0f);
+        floorGeo.addControl(floorPhy);
+        bulletAppState.getPhysicsSpace().add(floorPhy);
 
-        rootNode.attachChild(geom);
+        playerControl = new BetterCharacterControl(1.5f, 6f, 1f);
+
+        playerNode = new Node("PlayerNode");
+        playerNode.addControl(playerControl);
+
+        Box playerMesh = new Box(0.5f, 2.5f, 0.5f);
+        Geometry playerGeo = new Geometry("PlayerVisual", playerMesh);
+        Material playerMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        playerMat.setColor("Color", ColorRGBA.Blue);
+        playerGeo.setMaterial(playerMat);
+        playerNode.attachChild(playerGeo);
+
+        rootNode.attachChild(playerGeo);
+        bulletAppState.getPhysicsSpace().add(playerControl);
+
+        playerControl.setJumpForce(new Vector3f(0, 5f, 0));
+        playerControl.setGravity(new Vector3f(0,-4.9f,0));
+        playerControl.warp(new Vector3f(0,5,0));
+
+
+        flyCam.setMoveSpeed(0);
+        initKeys();
     }
 
 }
