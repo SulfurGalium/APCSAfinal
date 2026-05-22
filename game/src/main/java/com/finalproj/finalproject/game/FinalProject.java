f1f47e8cf06d85113c4b82aa9319eb91e495ed74
Aarush Jain <63690978+aarush1023@users.noreply.github.com>
2026-05-22 12:46:13 -0700

    Added crosshair and sprint
    Added crosshair using one horizontal and one vertical line. Added sprint. Maybe change speed at later date

--- game/src/main/java/com/finalproj/finalproject/game/FinalProject.java
+++ game/src/main/java/com/finalproj/finalproject/game/FinalProject.java
@@ -1,11 +1,6 @@
 package com.finalproj.finalproject.game;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
 import com.jme3.app.state.AppState;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.control.BetterCharacterControl;
@@ -15,7 +10,13 @@ import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Line;
 
 
 
@@ -46,14 +47,17 @@ public class FinalProject extends SimpleApplication implements ActionListener {
         inputManager.addMapping("Up",    new KeyTrigger(KeyInput.KEY_W));
         inputManager.addMapping("Down",  new KeyTrigger(KeyInput.KEY_S));
         inputManager.addMapping("Click", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+        inputManager.addMapping("Shift", new KeyTrigger(KeyInput.KEY_LSHIFT));
     
 
-        inputManager.addListener(this, "Jump", "Left", "Right", "Up", "Down", "Click");
+        inputManager.addListener(this, "Jump", "Left", "Right", "Up", "Down", "Click", "Shift");
     }
     
 
     private Vector3f walkDirection = new Vector3f();
-    private boolean left = false, right = false, up = false, down = false, click = false;
+    private boolean left = false, right = false, up = false, down = false, click = false, shift = false;
+    private final float WALK_SPEED = 10f;
+    private final float SPRINT_SPEED = 25f;
 
     @Override
     public void onAction(String name, boolean isPressed, float tpf) {
@@ -65,6 +69,7 @@ public class FinalProject extends SimpleApplication implements ActionListener {
         if (name.equals("Right")) { right = isPressed; }
         if (name.equals("Up"))    { up    = isPressed; }
         if (name.equals("Down"))  { down  = isPressed; }
+        if (name.equals("Shift")) {shift = isPressed;}
         if (name.equals("Click")) {
             click = isPressed;
             if (tool instanceof Grapple) {
@@ -84,18 +89,44 @@ public class FinalProject extends SimpleApplication implements ActionListener {
         Vector3f camDir = cam.getDirection().clone().setY(0).normalizeLocal();
         Vector3f camLeft = cam.getLeft().clone().setY(0).normalizeLocal();
         walkDirection.set(0, 0, 0);
+        float speed = WALK_SPEED;
     
         if (left)  { walkDirection.addLocal(camLeft); }
         if (right) { walkDirection.addLocal(camLeft.mult(-1f)); }
         if (down)  { walkDirection.addLocal(camDir.mult(-1f)); }
         if (up)    { walkDirection.addLocal(camDir); }
         if (click) { tool.shoot(camDir, playerNode);}
+        if (shift) {speed = SPRINT_SPEED;}
     
-        // This actually moves the physical character
-        playerControl.setWalkDirection(walkDirection.multLocal(15f)); 
+        // moves the physical character
+        playerControl.setWalkDirection(walkDirection.multLocal(speed)); 
         
         // Keep the camera locked to the player
         cam.setLocation(playerNode.getWorldTranslation().add(0, 4f, 0));
+    }
+
+    private void initCrosshairs() {
+        setDisplayStatView(false);
+        setDisplayFps(false);
+
+        float centerX = settings.getWidth() / 2f;
+        float centerY = settings.getHeight() / 2f;
+        float radius = 10f;
+
+        Line horizontalMesh = new Line(new Vector3f(centerX - radius, centerY, 0), new Vector3f(centerX + radius, centerY, 0));
+        Line verticalMesh = new Line(new Vector3f(centerX, centerY - radius, 0), new Vector3f(centerX, centerY + radius, 0));
+
+        Geometry horizontalGeom = new Geometry("HorizontalCrosshair", horizontalMesh);
+        Geometry verticalGeom = new Geometry("VerticalCrosshair", verticalMesh);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.White);
+
+        horizontalGeom.setMaterial(mat);
+        verticalGeom.setMaterial(mat);
+
+        guiNode.attachChild(horizontalGeom);
+        guiNode.attachChild(verticalGeom);
     }
     
 
@@ -104,7 +135,7 @@ public class FinalProject extends SimpleApplication implements ActionListener {
     public void simpleInitApp() {
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
-        Box floorBox = new Box(10f, 0.2f, 10f);
+        Box floorBox = new Box(40f, 0.2f, 40f);
         Geometry floorGeo = new Geometry("Floor", floorBox);
         Material floorMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         floorMat.setColor("Color", ColorRGBA.Blue);
@@ -131,7 +162,7 @@ public class FinalProject extends SimpleApplication implements ActionListener {
         rootNode.attachChild(playerNode);
         bulletAppState.getPhysicsSpace().add(playerControl);
 
-        playerControl.setJumpForce(new Vector3f(0, 15f, 0));
+        playerControl.setJumpForce(new Vector3f(0, 10f, 0));
         playerControl.setGravity(new Vector3f(0,-9.8f,0));
         playerControl.warp(new Vector3f(0,5,0));
 
@@ -142,6 +173,7 @@ public class FinalProject extends SimpleApplication implements ActionListener {
         tool = new Grapple(assetManager, bulletAppState, floorPhy, rootNode);
 
         initKeys();
+        initCrosshairs();
     }
 
 
